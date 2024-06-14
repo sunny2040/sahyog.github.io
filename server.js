17f7b228fb6938c4b7482b33b2ef673817113904
admin <sunny.kumarr010@gmail.com>
2024-06-14 16:28:48 +0530

    techvision

--- server.js
+++ server.js
@@ -38,9 +38,9 @@ app.post('/initiatePayment', async (req, res) => {
       merchantTransactionId: transactionId,
       merchantUserId: phone,
       amount: amount * 100, // Amount in paise
-      redirectUrl: `https://sunny2040.github.io/sahyog.github.io/success`,
+      redirectUrl: `https://sunny2040.github.io/sahyog.github.io:3000/success`,
       redirectMode: "REDIRECT",
-      callbackUrl: `https://sunny2040.github.io/sahyog.github.io/api/status/${transactionId}`,
+      callbackUrl: `https://sunny2040.github.io/sahyog.github.io:5000/api/status/${transactionId}`,
       mobileNumber: phone,
       paymentInstrument: {
         type: "PAY_PAGE",
